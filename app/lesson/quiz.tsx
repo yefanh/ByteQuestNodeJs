513625d0ec3c2feb2265ccfc48f069879ab054f4
Yefan He <hyf528@outlook.com>
2024-12-14 17:09:27 -0500

    challenge card

--- app/lesson/quiz.tsx
+++ app/lesson/quiz.tsx
@@ -1,9 +1,9 @@
 "use client";
 
-// import { challengeOptions, challenges, userSubscription } from "@/db/schema";
 import { challengeOptions, challenges} from "@/db/schema";
 import { useState, useTransition } from "react";
 import { Header } from "./header";
+import { QuestionBubble } from "./question-bubble";
 
 type Props ={
   initialPercentage: number;
@@ -13,9 +13,7 @@ type Props ={
     completed: boolean;
     challengeOptions: typeof challengeOptions.$inferSelect[];
   })[];
-  // userSubscription: typeof userSubscription.$inferSelect & {
-  //   isActive: boolean;
-  // } | null;
+  
   userSubscription: any;
 };
 
@@ -30,6 +28,18 @@ export const Quiz = ({
   const [percentage, setPercentage] = useState(() => {
     return initialPercentage === 100 ? 0 : initialPercentage;
   });
+
+  const [challenges] = useState(initialLessonChallenges);
+  const [activeIndex, setActiveIndex] = useState(() => {
+    const uncompletedIndex = challenges.findIndex((challenge) => !challenge.completed);
+    return uncompletedIndex === -1 ? 0 : uncompletedIndex;
+  });
+
+  const challenge = challenges[activeIndex];
+
+  const title = challenge.type === "ASSIST" 
+    ? "Select the correct answer" 
+    : challenge.question;
   
   return (
     <>
@@ -38,6 +48,20 @@ export const Quiz = ({
         percentage={percentage}
         hasActiveSubscription={!!userSubscription?.isActive}
       />
+      <div className="flex-1">
+        <div className="h-full flex items-center justify-center">
+          <div className="lg:min-h-[350px] lg:w-[600px] w-full px-6 lg:px-0 flex flex-col gap-y-12">
+            <h1 className="text-lg lg:text-3xl text-center lg:text-start font-bold text-neutral-700">
+              {title}
+            </h1>
+            <div>
+              {challenge.type === "ASSIST" && (
+                <QuestionBubble question={challenge.question} />
+              )}
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
